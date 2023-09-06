6cf799ba3b858cbdcdfe0e0b1b94d387ae6c3332
sam585456525 <sam585456525@gmail.com>
2023-09-06 20:43:19 +0800

    init

---
+++ prettier.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  singleQuote: true,
+  arrowParens: 'always',
+  bracketSpacing: true,
+  embeddedLanguageFormatting: 'auto',
+  htmlWhitespaceSensitivity: 'css',
+  insertPragma: false,
+  jsxBracketSameLine: false,
+  jsxSingleQuote: true,
+  printWidth: 80,
+  proseWrap: 'preserve',
+  semi: true,
+  tabWidth: 2,
+  useTabs: false,
+};
